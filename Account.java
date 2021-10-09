dac15eca3b81a7889f35d5dd7bba94387466171e
garrettadamsss <garrettadamssss@gmail.com>
2021-10-08 17:32:52 -0700

    adding account, stocker, manager file

---
+++ Account.java
@@ -0,0 +1,13 @@
+abstract class Account{
+    String username;
+    String password;
+    int ID; 
+
+    public Account(String username, String password, int ID){
+        this.username = username;
+        this.password = password;
+        this.ID = ID; 
+    }
+    
+
+}
---
+++ Stocker.java
@@ -0,0 +1,3 @@
+public class Stocker extends Account{
+    
+}
