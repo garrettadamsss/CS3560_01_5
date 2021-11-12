d4cba524e22336f89005b74bd0645537fe42cef5
Michael Hessler <mhessler735@gmail.com>
2021-11-11 18:31:02 -0800

    GUI update 2

--- loginForm/src/main/java/com/example/loginform/App.java
+++ loginForm/src/main/java/com/example/loginform/App.java
@@ -1,5 +1,8 @@
 package com.example.loginform;
 
+/**
+ * This class is used to build the .jar application
+ */
 public class App {
     public static void main(String[] args) {
         HelloApplication.main(args);
