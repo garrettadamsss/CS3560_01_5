9c0e753c84dc063ad7ec0bf345d1ec148c3e4139
Michael Hessler <mhessler735@gmail.com>
2021-11-12 23:56:51 -0800

    added Inventory folder

---
+++ InventorySystem/src/main/java/com/example/loginform/Login.java
@@ -0,0 +1,59 @@
+package com.example.loginform;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+/**
+ * This class is used to login to the inventory app.
+ */
+public class Login {
+
+    public Login() {
+
+    }
+
+    @FXML
+    private Button button;
+    @FXML
+    private Label wrongLogin;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+
+
+    public void userLogin(ActionEvent event) throws IOException {
+        checkLogin();
+
+    }
+
+    private void checkLogin() throws IOException {
+        GroceryApp m = new GroceryApp();
+        if(username.getText().equals("stocker") && password.getText().equals("123")) {
+            wrongLogin.setText("Success!");
+
+            m.changeScene("mainMenu.fxml");
+        }
+
+        else if(username.getText().isEmpty() && password.getText().isEmpty()) {
+            wrongLogin.setText("Please enter your data.");
+        }
+
+
+        else {
+            wrongLogin.setText("Wrong username or password!");
+        }
+    }
+
+
+}
